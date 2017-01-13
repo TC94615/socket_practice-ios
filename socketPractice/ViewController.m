968ed01a0f2703f8a147c07a69cffdf141e63084
TC94615 <tclee0117@gmail.com>
2017-01-13 16:46:32 +0800

    Layout.

--- socketPractice/ViewController.m
+++ socketPractice/ViewController.m
@@ -11,9 +11,48 @@
 
 @interface ViewController()
 
+@property (nonatomic, strong) UITextField *msgBox;
 @end
 
 @implementation ViewController
+- (void)loadView {
+    [super loadView];
+    UITextField *msgBox = [[UITextField alloc] initWithFrame:CGRectMake(0, 50, 100, 30)];
+    msgBox.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:msgBox];
+    self.msgBox = msgBox;
+
+    UIButton *sendBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    sendBtn.frame = CGRectMake(0, 100, 100, 25);
+    [sendBtn setTitle:@"Send" forState:UIControlStateNormal];
+    [sendBtn addTarget:self action:@selector(send) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:sendBtn];
+
+    UIButton *connectBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    connectBtn.frame = CGRectMake(0, 200, 100, 25);
+    [connectBtn setTitle:@"Connect" forState:UIControlStateNormal];
+    [connectBtn addTarget:self action:@selector(connect) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:connectBtn];
+
+    UIButton *disconnectBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    disconnectBtn.frame = CGRectMake(0, 300, 100, 25);
+    [disconnectBtn setTitle:@"Disconnect" forState:UIControlStateNormal];
+    [disconnectBtn addTarget:self action:@selector(disconnect) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:disconnectBtn];
+}
+
+- (void)send {
+
+}
+
+- (void)connect {
+
+}
+
+- (void)disconnect {
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
